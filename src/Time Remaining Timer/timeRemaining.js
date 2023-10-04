c557dd8ccad06e3729a5fd7e14acac6f8fc7c7fc
Nicholas Hanes <nicholas.s.hanes@systolic.com>
2023-10-04 15:28:52 -0400

    Added a time remaining dir w/ html, scss, and js files

---
+++ src/Time Remaining Timer/timeRemaining.js
@@ -0,0 +1,25 @@
+function startTimer(duration, display) {
+  var timer = duration, hours, minutes, seconds;
+  setInterval(function () {
+    hours = parseInt(timer / 3600, 10);
+    minutes = parseInt((timer % 3600) / 60, 10);
+    seconds = parseInt((timer % 3600) % 60, 10);
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = hours + ":" + minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = 0;
+    }
+  }, 1000);
+}
+
+window.unload = function () {
+  var display = document.querySelector('#timer');
+  var duration = 60 * 60 * 2; // 2 hours
+
+  startTimer(duration, display);
+};
